a3cb310bf82b9fd258c9f6852f14362a6dde3096
huoruiwen <ruiwen.rw@nonolive.com>
2022-04-04 20:24:49 +0800

    commit

--- middle/62_unique_paths.go
+++ middle/62_unique_paths.go
@@ -4,3 +4,26 @@
  * @Date 2022/4/4
  **/
 package main
+
+func swapPairs(head *ListNode) *ListNode {
+	i := 1
+	headNode := head
+	lastNode := head
+	for {
+		if headNode == nil {
+			break
+		}
+		if i%2 == 1 {
+			lastNode = headNode
+		} else {
+			// 交换值
+			lastNodeVal := lastNode.Val
+			lastNode.Val = headNode.Val
+			headNode.Val = lastNodeVal
+		}
+		headNode = headNode.Next
+		i++
+	}
+
+	return head
+}
