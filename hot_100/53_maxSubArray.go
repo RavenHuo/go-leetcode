c6951fa05602cdb62698818cc4d56100356e1aff
raven <ravenhuo@olaparty.sg>
2025-05-09 11:06:54 +0800

    hot

--- hot_100/53_maxSubArray.go
+++ hot_100/53_maxSubArray.go
@@ -1,50 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
 // 动态规划
 // 53. 最大子数组和
-// 状态转移方程：当前元素重新开始或者跟之前的合并为一个子数组
+// 当前下标的最大和就是当前n+前
 func maxSubArray(nums []int) int {
-	if len(nums) == 0 {
-		return 0
-	}
-	resultList := make([]int, len(nums))
-	resultList[0] = nums[0]
-	m := nums[0]
-	for i := 1; i <= len(nums)-1; i++ {
+	arr := make([]int, len(nums))
+	arr[0] = nums[0]
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
 		n := nums[i]
-		resultList[i] = maxNum(n, resultList[i-1]+n)
-		if resultList[i] > m {
-			m = resultList[i]
-		}
+		arr[i] = maxNum(n, arr[i-1]+n)
+		max = maxNum(arr[i], max)
 	}
-
-	return m
+	return max
 }
 
-func maxNum(n, m int) int {
-	if n > m {
-		return n
+func maxNum(a, b int) int {
+	if a > b {
+		return a
 	}
-	return m
-}
-
-func main() {
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		time.Sleep(time.Second)
-	}()
-	go func() {
-		wg.Wait()
-		fmt.Println("go wg wait ")
-	}()
-	fmt.Println("main wg wait ")
-	time.Sleep(time.Second)
+	return b
 }
